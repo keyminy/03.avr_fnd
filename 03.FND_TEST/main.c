6c93958a19f0ecb77b198dad77b77576971f8d63
keyminy <imanaifs@gmail.com>
2024-04-26 13:16:06 +0900

    anode, fixed bug

--- 03.FND_TEST/main.c
+++ 03.FND_TEST/main.c
@@ -39,7 +39,7 @@ void init_fnd(void){
 	FND_DATA_DDR = 0xFF;
 	FND_DIGIT_DDR |= 0xF0;// 4,5,6,7만 출력이니까 거기만 write모드
 	
-	#if 0 //common anode방식으로
+	#if 1 //common anode방식으로
 		FND_DATA_PORT = ~0x00; // common anode : FND를 all off
 	#else	// common cathode
 		FND_DATA_PORT = 0x00; // PORTC 설정 : FND를 all off
@@ -50,25 +50,19 @@ void init_fnd(void){
 // display the FND
 void fnd_display(void){
 	
-	#if 0 // common anode
+	#if 1 // common anode
 		// 0을 찍을려면 g빼고 1로 16진수값 3F입니다, b는 0011_1111(common anode)
 		// common cathode는 1100_0000이고, 16진수로 C0
-		unsigned char fnd_font[] = {
-			// 0    1     2     3    4    5    6    7    8    9
-			0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xD8, 0x80, 0x98
-		};
+									//0    1     2     3    4    5    6    7    8    9
+		unsigned char fnd_font[] = {0xc0, 0xf9, 0xa4, 0xb0,0x99,0x92,0x82,0xd8,0x80,0x98,0x00};
 	#else
-		unsigned char fnd_font[] = {
-			// 0    1     2     3    4    5    6    7    8    9
-			~0xC0, ~0xF9, ~0xA4, ~0xB0, ~0x99, ~0x92, ~0x82, ~0xD8, ~0x80, ~0x98
-		};		
+		unsigned char fnd_font[] = {~0xc0, ~0xf9, ~0xa4, ~0xb0,~0x99,~0x92,~0x82,~0xd8,~0x80,~0x98,~0x00};
 	#endif
-	
-	static int digit_position = 0; // 자리수 선택 변수 0~3 : 0,1,2,3
+		static int digit_position = 0; // 자리수 선택 변수 0~3 : 0,1,2,3
 		
 	switch(digit_position){
 		case 0:
-			#if 0
+			#if 1
 				FND_DIGIT_PORT = 0b10000000; // anode
 			#else 
 				FND_DIGIT_PORT = ~0b10000000; // cathode
@@ -76,15 +70,15 @@ void fnd_display(void){
 				FND_DATA_PORT = fnd_font[8]; // 8번 index write = 9을 사용함
 			break;
 		case 1:
-			#if 0
-			FND_DIGIT_PORT = 0b10000000; // anode
+			#if 1
+			FND_DIGIT_PORT = 0b01000000; // anode
 			#else
-			FND_DIGIT_PORT = ~0b10000000; // cathode
+			FND_DIGIT_PORT = ~0b01000000; // cathode
 			#endif
 			FND_DATA_PORT = fnd_font[8]; // 8번 index write = 9을 사용함
 			break;
 		case 2:
-			#if 0
+			#if 1
 			FND_DIGIT_PORT = 0b00100000; // anode
 			#else
 			FND_DIGIT_PORT = ~0b00100000; // cathode
@@ -92,7 +86,7 @@ void fnd_display(void){
 			FND_DATA_PORT = fnd_font[8];// 8번 index write = 9을 사용함
 			break;
 		case 3:
-			#if 0
+			#if 1
 			FND_DIGIT_PORT = 0b00010000; // anode
 			#else
 			FND_DIGIT_PORT = ~0b00010000; // cathode
